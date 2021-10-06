8eb40a3bb39df88a1eb6d41a4f036f7888c5242a
Oleksii Shulzhenko <lexa270@gmail.com>
2021-10-06 23:28:04 +0300

    Minor fixes

--- bot_core.py
+++ bot_core.py
@@ -9,6 +9,6 @@ class Message:
         languages = ["en", "ru", "uk"]
         url_re = r"\b(?:https?://|www\.)[a-z0-9-]+(\.[a-z0-9-]+)+(?:[/?].*)?"
         new_text = re.sub(url_re, "", text)
-        return langid.classify(new_text)
+        return str(langid.classify(new_text))
         
 
