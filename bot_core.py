3ae88cc6c8fb43e954bbffae6ac0a4b403de0b42
Oleksii Shulzhenko <lexa270@gmail.com>
2021-10-06 23:25:07 +0300

    Minor fixes

--- bot_core.py
+++ bot_core.py
@@ -2,18 +2,6 @@
 import re
 import langid
 
-from langdetect import DetectorFactory, detect, detect_langs
-# class Message:
-#     def check_language(self,text):
-#         languages = ["en", "ru", "uk"]
-#         url_re = r"\b(?:https?://|www\.)[a-z0-9-]+(\.[a-z0-9-]+)+(?:[/?].*)?"
-#         new_text = re.sub(url_re, "", text)
-#         langs = detect_langs(new_text)  
-#         for language in langs:
-#             if language.lang in languages:
-#                 return  language
-#             else:
-#                 return "Undefined"
 
 
 class Message:
