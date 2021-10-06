557370d68793ec8119c43e4ef96e6a14ae10dea1
Oleksii Shulzhenko <lexa270@gmail.com>
2021-10-06 23:22:32 +0300

    Minor fixes

--- bot_core.py
+++ bot_core.py
@@ -1,19 +1,26 @@
-import gcld3
-import re
 
+import re
+import langid
+
+from langdetect import DetectorFactory, detect, detect_langs
 # class Message:
 #     def check_language(self,text):
+#         languages = ["en", "ru", "uk"]
 #         url_re = r"\b(?:https?://|www\.)[a-z0-9-]+(\.[a-z0-9-]+)+(?:[/?].*)?"
 #         new_text = re.sub(url_re, "", text)
-#         lang = cld3.get_language(new_text).language
-#         return str(lang)
+#         langs = detect_langs(new_text)  
+#         for language in langs:
+#             if language.lang in languages:
+#                 return  language
+#             else:
+#                 return "Undefined"
+
 
 class Message:
     def check_language(self,text):
         languages = ["en", "ru", "uk"]
-        detector = gcld3.NNetLanguageIdentifier(min_num_bytes=10, max_num_bytes=1000)
-        result = detector.FindTopNMostFreqLangs(text=text, num_langs=5)
-        for i in result:
-            if i.language in languages:
-                return str(i.language)
-            else: return "undefined"
+        url_re = r"\b(?:https?://|www\.)[a-z0-9-]+(\.[a-z0-9-]+)+(?:[/?].*)?"
+        new_text = re.sub(url_re, "", text)
+        return langid.classify(new_text)
+        
+
