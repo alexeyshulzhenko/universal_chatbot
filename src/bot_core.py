48802a85aed35d0e8e253c62696c851a6cfe8840
Oleksii Shulzhenko <lexa270@gmail.com>
2021-10-08 09:43:38 +0300

    Added basic Mono API requests to make my bot a little bit usable

--- src/bot_core.py
+++ src/bot_core.py
@@ -2,6 +2,7 @@
 import re
 import os
 import langid
+
 from src.mono_api import MonoAPI
 
 
@@ -15,10 +16,20 @@ class Message:
         return str(langid.classify(new_text))
         
 
+    def pick_command_en(self, text):
+        if (text == "exchange rate"):
+            return self.mono_api.get_exchange_rate()
+        elif (text == "balance"):
+            return self.mono_api.get_balance()
+        else: return "Err"
+
+
     def process_message(self, text, user, admin_user_id):
         if user == admin_user_id:
             language = self.check_language(text).split(',')[0].replace("'", "").replace("(", "")
-            return language
+            if language == "en":
+                answer = self.pick_command_en(text)
+            return answer
         else: 
             return "You are not authorised"
 
--- src/mono_api.py
+++ src/mono_api.py
@@ -1,17 +1,39 @@
 import requests
-import os
+import sys
+import os, sys
+import pandas as pd
 
 class MonoAPI:
 
     token = str(os.environ['T_TOKEN'])
+    
     basic_url = "https://api.monobank.ua/"
+    
+    iso_4217 = pd.read_csv("./static/iso-4217.csv")
+    
+    
 
 # Public API does not require authentefocation
     def get_exchange_rate(self):
         url = self.basic_url + "/bank/currency"
-        response = requests.get(url).text
-        return response
+        response = requests.get(url).json()
+        if len(response) > 0 :
+            response_df = pd.json_normalize(response)
+            response_df["date"] = pd.to_datetime(response_df["date"],unit="s") 
+            rate_line =  response_df.sort_values(by='date', ascending=False).head(1)
+            cur_a = self.iso_4217[self.iso_4217["NumericCode"] == int(rate_line["currencyCodeA"])]["AlphabeticCode"].item()
+            cur_b = self.iso_4217[self.iso_4217["NumericCode"] == int(rate_line["currencyCodeB"])]["AlphabeticCode"].item()
+            date = str(rate_line["date"].item())
+            sel_rate = str(rate_line["rateSell"].item())
+            buy_rate = str(rate_line["rateBuy"].item())
+            return "Rate for " + cur_a + " to " + cur_b + " for " +  date + " is: sell " + sel_rate + " buy " + buy_rate
+        else:
+            return "Please try again later"
 
 # Private API require MONO Token
     def get_balance(self):
-        return "test"
+        url = self.basic_url + "/personal/client-info"
+        headers = {'X-Token': self.token}
+        response = requests.get(url, headers=headers).json()
+
+        return "Hello"
